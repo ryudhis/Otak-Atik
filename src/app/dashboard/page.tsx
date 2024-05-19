04aa0a085b929e71821997ba499860796ffe5237
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-05-19 15:51:20 +0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import tech from "@svg/tech.svg";
 
 export interface dashboard {
   nama: string;
-  ownerId: number;
+  id: number;
   owner: ownerItem;
 }
 
@@ -42,7 +42,7 @@ const Dashboard = () => {
   return (
     <div className="bg-tertiary p-28 h-screen">
       <div className="flex flex-col gap-12">
-        <div className="flex gap-80">
+        <div className="flex gap-[410px]">
           <h1 className="text-4xl font-bold">Kelas Tech</h1>
           <div className="flex gap-4">
             <Button alternateStyle="text-white bg-tertiary border-secondary border-[2px] py-1 px-3">
@@ -53,25 +53,39 @@ const Dashboard = () => {
             </Button>
           </div>
         </div>
-        <div className="flex">
-          {dashboard.slice(0, 3).map((item) => (
-            <div
-              key={item.ownerId}
-              className="p-6 w-56 h-80 border-2 border-primary rounded-3xl flex flex-col gap-8"
-            >
-              <div className="flex justify-between items-center">
-                <h1>{item.owner.username}</h1>
-                <Button alternateStyle="bg-transparent hover:bg-transparent active:bg-transparent">
-                  <Image src={Star} alt="" />
-                </Button>
-              </div>
-              <div className="flex flex-col justify-center items-center gap-4">
-                <Image src={tech} alt="" />
-                <h1 className="text-lg font-bold text-center">{item.nama}</h1>
-              </div>
+        {isLoading ? (
+          <div className="flex gap-5">
+            <div className="p-6 w-56 h-80 border-2 border-primary rounded-3xl animate-pulse flex justify-center items-center">
+              <h1 className="text-center animate-pulse">Loading class..</h1>
             </div>
-          ))}
-        </div>
+            <div className="p-6 w-56 h-80 border-2 border-primary rounded-3xl animate-pulse flex justify-center items-center">
+              <h1 className="text-center animate-pulse">Loading class..</h1>
+            </div>
+            <div className="p-6 w-56 h-80 border-2 border-primary rounded-3xl animate-pulse flex justify-center items-center">
+              <h1 className="text-center animate-pulse">Loading class..</h1>
+            </div>
+          </div>
+        ) : (
+          <div className="flex gap-5">
+            {dashboard.slice(0, 3).map((item) => (
+              <div
+                key={item.id}
+                className="p-6 w-56 h-80 border-2 border-primary rounded-3xl flex flex-col gap-8"
+              >
+                <div className="flex justify-between items-center">
+                  <h1>{item.owner.username}</h1>
+                  <Button alternateStyle="bg-transparent hover:bg-transparent active:bg-transparent">
+                    <Image src={Star} alt="" />
+                  </Button>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-4">
+                  <Image src={tech} alt="" />
+                  <h1 className="text-lg font-bold text-center">{item.nama}</h1>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
