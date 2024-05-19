5ef355d6c397c7e41669f551c778801a7de6d949
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-05-19 15:27:32 +0700

    fix dummy class highlight

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Button from "../components/Button";
+import Star from "@svg/star.svg";
+import Image from "next/image";
+import tech from "@svg/tech.svg";
 
 const Dashboard = () => {
   return (
@@ -17,7 +20,20 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="flex">
-          <div className="px-36 py-44 border-2 border-primary rounded-3xl"></div>
+          <div className="p-6 w-56 h-80 border-2 border-primary rounded-3xl flex flex-col gap-8">
+            <div className="flex justify-between items-center">
+              <h1>Rizky</h1>
+              <Button alternateStyle="bg-transparent hover:bg-transparent active:bg-transparent">
+                <Image src={Star} alt="" />
+              </Button>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-4">
+              <Image src={tech} alt="" />
+              <h1 className="text-lg font-bold text-center">
+                Data Visualization dengan Python
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
