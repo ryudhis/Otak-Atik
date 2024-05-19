b1b2b540c3863742b31ed67ac7cc98342fa5bc84
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-05-19 15:14:52 +0700

    add dummy class highlight

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import Button from "../components/Button";
 
 const Dashboard = () => {
   return (
-    <div className="bg-tertiary p-4 h-screen">
-      <div className="flex flex-col">
-        <div className="flex">
-          <h1 className="">Kelas Tech</h1>
+    <div className="bg-tertiary p-28 h-screen">
+      <div className="flex flex-col gap-12">
+        <div className="flex gap-80">
+          <h1 className="text-4xl font-bold">Kelas Tech</h1>
+          <div className="flex gap-4">
+            <Button alternateStyle="text-white bg-tertiary border-secondary border-[2px] py-1 px-3">
+              &lt;
+            </Button>
+            <Button alternateStyle="py-1 px-3 border-secondary border-[2px]">
+              &gt;
+            </Button>
+          </div>
         </div>
-        <div className="flex"></div>
+        <div className="flex">
+          <div className="px-36 py-44 border-2 border-primary rounded-3xl"></div>
+        </div>
       </div>
     </div>
   );
