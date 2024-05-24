62f9be63721228c6959ba506df9ff1b70903c4fc
Aziz Mujahiddin Nugraha <azizmn03@gmail.com>
2024-05-24 16:10:41 +0700

    Update Header.tsx
    Create link to faq

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -11,7 +11,7 @@ const Accordion = ({ title, answer }) => {
                 {accordionOpen ? <span>-</span> : <span>+</span>}
             </button>
             <div
-                className={`mb-2 grid overflow-hidden transition-all duration-300 ease-in-out text-slate-300 text-m ${
+                className={`mb-2 grid overflow-hidden transition-all duration-300 text-secondary ease-in-out text-m ${
                     accordionOpen
                     ? "grid-rows-[1fr] opacity-100"
                     : "grid-rows-[0fr] opacity-0"
